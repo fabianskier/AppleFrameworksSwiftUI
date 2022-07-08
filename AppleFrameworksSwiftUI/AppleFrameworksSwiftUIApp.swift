65622413ca10824bcdfc92a13c65c0fb096486c7
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-08 15:22:50 -0400

    Initial Commit

---
+++ AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksSwiftUIApp.swift
+//  AppleFrameworksSwiftUI
+//
+//  Created by Oscar Cristaldo on 2022-07-08.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
